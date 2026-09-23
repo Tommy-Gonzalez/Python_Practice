06f8fbfdb0ffcf503a175d22fbde4746642e65a1
Tommy-Gonzalez <TommyGonzo@protonmail.com>
2026-09-23 17:28:07 -0400

    Add files via upload

---
+++ Chapter_3/Slope program.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#This program calculates the slope of a line that passes through two points
+
+x1 = int(input("Enter your first x coordinate: "))
+y1 = int(input("Enter your first y coordinate: "))
+
+x2 = int(input("Enter your second x coordinate: "))
+y2 = int(input("Enter your second y coordinate: "))
+
+slope = (y2 - y1)/(x2-x1)
+
+print("First coordinate points: ", x1, y1)
+print("Second coordinate points: ", x2, y2)
+
+print("The slope of the line between your two points is: ", slope)
